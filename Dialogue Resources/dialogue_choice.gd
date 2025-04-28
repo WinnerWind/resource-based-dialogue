5096d4c7559b471d61d3135d35e1f1e46be035ab
WinnerWind <71366717+WinnerWind@users.noreply.github.com>
2025-04-28 22:21:06 +0530

    Toolify dialogue choice

--- Dialogue Resources/dialogue_choice.gd
+++ Dialogue Resources/dialogue_choice.gd
@@ -1,5 +1,13 @@
+@tool
 extends Resource
 class_name DialogueChoice
 
-@export var text:String
+@export var text:String:
+	set(new_text):
+		text = new_text
+		_set_resource_name()
+
 @export var dialogue_to_start:DialogueAct
+
+func _set_resource_name():
+	resource_name = text
