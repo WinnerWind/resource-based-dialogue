9fa25efe5251e61ad91e47cb8a61db5c780b3430
WinnerWind <71366717+WinnerWind@users.noreply.github.com>
2025-04-28 22:19:34 +0530

    Toolify the resources.

--- Dialogue Resources/dialogue_act.gd
+++ Dialogue Resources/dialogue_act.gd
@@ -1,5 +1,11 @@
+@tool
 extends Resource
 class_name DialogueAct
 
 @export var dialogue_lines:Array[DialogueLine]
 @export var choices:Array[DialogueChoice]
+@export var is_at_end:bool
+
+func _validate_property(property: Dictionary) -> void:
+	if property.name == "choices" and is_at_end:
+		property.usage = PROPERTY_USAGE_NO_EDITOR
--- Dialogue Resources/dialogue_line.gd
+++ Dialogue Resources/dialogue_line.gd
@@ -1,5 +1,20 @@
+@tool
 extends Resource
 class_name DialogueLine
 
-@export var custom_speaker_name:String
-@export_multiline var text:String
+@export var custom_speaker_name:String:
+	set(new_text):
+		custom_speaker_name = new_text
+		_set_resource_name()
+
+@export_multiline var text:String:
+	set(new_text):
+		text = new_text
+		_set_resource_name()
+
+var speaker_name:
+	get:
+		return custom_speaker_name
+
+func _set_resource_name():
+	resource_name = speaker_name + " - " + text
